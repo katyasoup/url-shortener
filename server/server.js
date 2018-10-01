b8f09dcdfc3de95944a54b75b1d64ddbc7227e71
katyasoup <campbell.katharine@gmail.com>
2018-10-01 10:07:43 -0500

    update bootstrap and .gitignore

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use('/api/user', userRouter);
 // Serve static files
 app.use(express.static('server/public'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 /** Listen * */
 app.listen(PORT, () => {
